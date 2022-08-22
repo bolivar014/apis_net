b8e99f5f368683efa69fb11f89858844f6e642f2
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-08-22 16:14:09 -0500

    creando api tipo post para agregar nuevo registro a api weaterforecast

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,20 +13,38 @@ public class WeatherForecastController : ControllerBase
 
     private readonly ILogger<WeatherForecastController> _logger;
 
+    // Inicializamos la creación de lista 
+    private static List<WeatherForecast> ListWeatherForecast = new List<WeatherForecast>();
+
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+
+        // Validamos si la lista es igual a null ó no tiene ningun registro
+        if(ListWeatherForecast == null || !ListWeatherForecast.Any()) {
+            ListWeatherForecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToList();
+        }
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        // Retornamos lista
+        return ListWeatherForecast;
+    }
+
+    [HttpPost]
+    public IActionResult Post(WeatherForecast weatherForecast) {
+        // Agregamos registro al modelo que estamos recibiendo
+        ListWeatherForecast.Add(weatherForecast);
+
+        // Retornamos OK cuando sucede su ejecución
+        return Ok();
     }
 }
