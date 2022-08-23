13dd8c33021296fe0be1002b175a1616eb04b10e
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-08-22 21:43:16 -0500

    integrando creación de servicios - CRUD Tarea

--- Services/TareasService.cs
+++ Services/TareasService.cs
@@ -13,8 +13,38 @@ public class TareasService : ITareasService{
     public IEnumerable<Tarea> Get() {
         return context.Tareas;
     }
+
+    // Generamos la creación de una nueva tarea
+    public async Task Save(Tarea tarea) {
+        context.Add(tarea);
+        await context.SaveChangesAsync();
+    }
+    // Generamos la actualización de una Tarea
+    public async Task Update(Guid id, Tarea tarea) {
+        var tareaActual = context.Tareas.Find(id);
+
+        if(tareaActual != null) {
+            tareaActual.Descripcion = tarea.Descripcion;
+            tareaActual.Resumen = tarea.Resumen;
+            tareaActual.PrioridadTarea = tarea.PrioridadTarea;
+
+            await context.SaveChangesAsync();
+        }
+    }
+    // Generamos la Eliminación de una Tarea
+    public async Task Delete(Guid id) {
+        var tareaActual = context.Tareas.Find(id);
+
+        if(tareaActual != null) {
+            context.Remove(tareaActual);
+            await context.SaveChangesAsync();
+        }
+    }
 }
 
 public interface ITareasService {
     IEnumerable<Tarea> Get();
+    Task Save(Tarea tarea);
+    Task Update(Guid id, Tarea tarea);
+    Task Delete(Guid id);
 }
